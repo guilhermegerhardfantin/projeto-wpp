900f63b7bb7d01bfb0ac984a67e2fce937a309a8
guilherme <guilhermeguerhardfantin@gmail.com>
2024-10-24 17:32:11 -0300

    4

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 $(".phone_with_ddd").mask("(00) 00000-0000");
 let form = document.querySelector("#gerar-link");
 let input = document.querySelector("#whatsapp");
+let mensagem = document.querySelector("#mensagem");
 let linkGerado = document.querySelector("#link-gerado");
 let boxGerado = document.querySelector("#box-gerado");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   let numeroFormatado = "55" + input.value.replace(/[^\d]/g, "");
+  let mensagemFormatada = "";
+  if (mensagem.value.trim() !== "") {
+    mensagemFormatada = encodeURIComponent(mensagem.value.trim());
+
+  }
   linkGerado.textContent =
-    "https://api.whatsapp.com/send?phone=" + numeroFormatado;
-  linkGerado.href = "https://api.whatsapp.com/send?phone=" + numeroFormatado;
+    "https://api.whatsapp.com/send?phone=" +
+    numeroFormatado +
+    (mensagemFormatada ? "&text=" + mensagemFormatada : "");
+
+  linkGerado.href =
+    "https://api.whatsapp.com/send?phone=" +
+    numeroFormatado +
+    (mensagemFormatada ? "&text=" + mensagemFormatada : "");
+    
   form.reset();
   boxGerado.style.display = "block";
 });
-
